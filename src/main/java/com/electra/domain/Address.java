81bfb55c940127989a8e95da5353d28bdfcfa1b7
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-21 18:28:26 +0530

    commits

---
+++ src/main/java/com/electra/domain/Address.java
@@ -0,0 +1,36 @@
+package com.electra.domain;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Builder
+@Data  // Lombok annotation to generate getters, setters, toString, etc.
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name ="address")
+
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "street" , nullable = false)
+    private String street;
+
+    @Column(name = "city" , nullable = false)
+    private String city;
+
+    @Column(name = "state" , nullable = false)
+    private String state;
+
+    @Column(name = "country" , nullable = false)
+    private String country;
+
+    @Column(name = "postal_code" , nullable = false)
+    private long postalCode;
+}
+
+
+

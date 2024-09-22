c0c2014bafcf69468e0036cef55aeae4473f7adc
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-22 10:57:16 +0530

    commits

---
+++ src/main/java/com/electra/domain/Brand.java
@@ -0,0 +1,23 @@
+package com.electra.domain;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Data // Lombok annotation to generate getters, setters, toString, etc.
+@Entity
+@Table(name = "brand")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name" , nullable = false)
+    private String name;
+
+    @Column(name = "description" , nullable = false)
+    private String description;
+}

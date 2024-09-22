1272e9096e36960ad310289a6c48349ce14acfeb
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-22 10:58:14 +0530

    commits

---
+++ src/main/java/com/electra/domain/Order.java
@@ -0,0 +1,32 @@
+package com.electra.domain;
+
+
+import java.time.LocalDate;
+import lombok.*;
+import javax.persistence.*;
+
+@Data // Lombok annotation to generate getters, setters, toString, etc.
+@Entity
+@Table(name = "order")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "product" , nullable = false , unique = true , updatable = false)
+    private Product product;
+
+    @Column(name = "customer_details" , nullable = false , unique = true , updatable = false)
+    private Customer customer;
+
+    @Column(name = "supplier_details" , nullable = false , unique = true , updatable = false )
+    private Supplier supplier;
+
+    @Column(name = "order_date" , nullable = false , updatable = false)
+    private LocalDate orderDate;
+}

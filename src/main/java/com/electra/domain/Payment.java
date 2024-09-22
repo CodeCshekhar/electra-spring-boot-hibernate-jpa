7ddfa3112f3dcb4206f5c93ae0cbb2a4f90dad90
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-22 10:58:35 +0530

    commits

---
+++ src/main/java/com/electra/domain/Payment.java
@@ -0,0 +1,32 @@
+package com.electra.domain;
+
+import java.time.LocalDate;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data // Lombok annotation to generate getters, setters, toString, etc.
+@Entity
+@Table(name = "payment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Payment{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "amount" , nullable = false , updatable = false)
+    private Double amount;
+
+    @Column(name = "payment_date" , nullable = false , updatable = false)
+    private LocalDate paymentDate;
+
+    @Column(name = "customer_details" , nullable = false , updatable = false)
+    private Customer customer;
+
+    @Column(name =  "order_details" , nullable = false ,updatable = false)
+    private Order order;
+}

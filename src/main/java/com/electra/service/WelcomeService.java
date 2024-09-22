4d6cd530645468d70d265fc1c680877379a402cc
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-22 10:59:07 +0530

    commits

---
+++ src/main/java/com/electra/service/WelcomeService.java
@@ -0,0 +1,14 @@
+package com.electra.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WelcomeService {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    public String retrieveWelcomeMessage() {
+        return welcomeMessage;
+    }
+}

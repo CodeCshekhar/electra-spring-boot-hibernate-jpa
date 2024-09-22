ba3bad4cb910ef00404ec669c7409878ccc700d3
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-22 10:58:30 +0530

    commits

---
+++ src/main/java/com/electra/service/OrderService.java
@@ -0,0 +1,51 @@
+package com.electra.service;
+
+import com.electra.domain.Order;
+import com.electra.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class OrderService {
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        logger.info("Orders Retrieved in Services");
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(Long id) {
+        logger.info("Order detected by ID in Services");
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public Order createOrder(Order order) {
+        logger.info("Order Created in Services");
+        return orderRepository.save(order);
+    }
+
+    public Order updateOrder(Long id, Order updatedOrder) {
+        Order order = orderRepository.findById(id).orElse(null);
+        if (order != null) {
+            order.setProduct(updatedOrder.getProduct());
+            order.setCustomer(updatedOrder.getCustomer());
+            order.setSupplier(updatedOrder.getSupplier());
+            order.setOrderDate(updatedOrder.getOrderDate());
+            logger.info("Order Updated in Services");
+            return orderRepository.save(order);
+        }
+        return null;
+    }
+
+    public void deleteOrder(Long id) {
+        logger.info("Order deleted");
+        orderRepository.deleteById(id);
+    }
+
+}

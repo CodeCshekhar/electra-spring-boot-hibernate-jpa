73ef94614fd1b859671f8021a28d79bd3fd8e15f
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-22 10:57:57 +0530

    commits

---
+++ src/main/java/com/electra/controller/CustomerController.java
@@ -0,0 +1,61 @@
+package com.electra.controller;
+
+import com.electra.domain.Address;
+import com.electra.domain.Brand;
+import com.electra.domain.Customer;
+import com.electra.service.BrandService;
+import com.electra.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/electra/customer")
+public class CustomerController {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        logger.info("Retrieving Customers from Get in Controller");
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+        logger.info("Retrieving Customer from Get by ID in Controller");
+        Customer customer = customerService.getCustomerById(id);
+        if (customer != null) {
+            return ResponseEntity.ok(customer);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        logger.info("Creating Customer from Post in Controller");
+        return customerService.createCustomer(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        logger.info("Updating Customer from Put in Controller");
+        Customer updatedCustomer = customerService.updateCustomer(id, customer);
+        if (updatedCustomer != null) {
+            return ResponseEntity.ok(updatedCustomer);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        logger.info("Deleting Customer from delete in Controller");
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+}

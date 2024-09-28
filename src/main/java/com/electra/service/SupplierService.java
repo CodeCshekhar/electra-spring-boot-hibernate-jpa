ff637c4207eb355aa76a789b6d72b702b7acca54
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-28 11:50:34 +0530

    commits

--- src/main/java/com/electra/service/SupplierService.java
+++ src/main/java/com/electra/service/SupplierService.java
@@ -5,6 +5,7 @@ import com.electra.repository.SupplierRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,32 +15,39 @@ public class SupplierService {
     private static final Logger logger = LoggerFactory.getLogger(SupplierService.class);
 
     @Autowired
-    private SupplierRepository repo;
+    private SupplierRepository supplierRepository;
 
-    public List<Supplier> retrieveSuppliers() {
-        logger.info("/inside the SupplierService.retrieveSuppliers()");
-        return repo.retrieve();
+    @Cacheable("getAllSuppliers")
+    public List<Supplier> getAllSuppliers() {
+        logger.info("Suppliers Retrieved in Services");
+        return supplierRepository.findAll();
     }
 
-    public String storeSupplier(Supplier supplier) {
-        logger.info("/inside the SupplierService.addSupplier()");;
-        return repo.store(supplier);
+    @Cacheable("getSupplierById")
+    public Supplier getSupplierById(Long id) {
+        logger.info("Supplier detected by ID in Services");
+        return supplierRepository.findById(id).orElse(null);
     }
 
-    public String deleteSupplier(int id) {
-        logger.info("/inside the SupplierService.removeSupplier()");
-        return repo.delete(id);
+    public Supplier createSupplier(Supplier supplier) {
+        logger.info("Supplier Created in Services");
+        return supplierRepository.save(supplier);
     }
 
-    public String search(int id) {
-        logger.info("/inside the SupplierService.search()");
-        return repo.search(id);
+    public Supplier updateSupplier(Long id, Supplier updatedSupplier) {
+        Supplier supplier = supplierRepository.findById(id).orElse(null);
+        if (supplier != null) {
+            supplier.setName(updatedSupplier.getName());
+            supplier.setContactInfo(updatedSupplier.getContactInfo());
+            logger.info("Supplier Updated in Services");
+            return supplierRepository.save(supplier);
+        }
+        return null;
     }
 
-    public String updateSupplier(int id, Supplier supplier) {
-        logger.info("/inside the SupplierService.updateSupplier()");
-        supplier.setId(id);
-        return repo.update(supplier);
+    public void deleteSupplier(Long id) {
+        logger.info("Payment deleted");
+        supplierRepository.deleteById(id);
     }
 
 }

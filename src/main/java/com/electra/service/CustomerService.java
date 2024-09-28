096ced7277e7e95d79ab6f2fc4fbdc32faaaa455
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-28 11:49:53 +0530

    commits

--- src/main/java/com/electra/service/CustomerService.java
+++ src/main/java/com/electra/service/CustomerService.java
@@ -5,6 +5,7 @@ import com.electra.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -15,11 +16,13 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Cacheable("getAllCustomers")
     public List<Customer> getAllCustomers() {
         logger.info("Customers Retrieved in Services");
         return customerRepository.findAll();
     }
 
+    @Cacheable("getCustomerById")
     public Customer getCustomerById(Long id) {
         logger.info("Customer detected by ID in Services");
         return customerRepository.findById(id).orElse(null);

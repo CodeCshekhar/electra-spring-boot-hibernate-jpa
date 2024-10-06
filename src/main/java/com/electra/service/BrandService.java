4e0de2cec9d93faa0cb83728ed83498d1b91b9c4
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-06 15:07:03 +0530

    commits

--- src/main/java/com/electra/service/BrandService.java
+++ src/main/java/com/electra/service/BrandService.java
@@ -2,50 +2,30 @@ package com.electra.service;
 
 import com.electra.domain.Brand;
 import com.electra.repository.BrandRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BrandService {
-    private static final Logger logger = LoggerFactory.getLogger(BrandService.class);
-
     @Autowired
     private BrandRepository brandRepository;
 
-    @Cacheable("getAllBrands")
-    public List<Brand> getAllBrands() {
-        logger.info("Brands Retrieved in Services");
+    public List<Brand> findAll() {
         return brandRepository.findAll();
     }
 
-    @Cacheable("getBrandById")
-    public Brand getBrandById(Long id) {
-        logger.info("Brand detected by ID in Services");
-        return brandRepository.findById(id).orElse(null);
+    public Optional<Brand> findById(Long id) {
+        return brandRepository.findById(id);
     }
 
-    public Brand createBrand(Brand brand) {
-        logger.info("Brand Created in Services");
+    public Brand save(Brand brand) {
         return brandRepository.save(brand);
     }
 
-    public Brand updateBrand(Long id, Brand updatedBrand) {
-        Brand brand = brandRepository.findById(id).orElse(null);
-        if (brand != null) {
-            brand.setName(updatedBrand.getName());
-            brand.setDescription(updatedBrand.getDescription());
-            logger.info("Brand Updated in Services");
-            return brandRepository.save(brand);
-        }
-        return null;
-    }
-
-    public void deleteBrand(Long id) {
-        logger.info("User deleted");
+    public void delete(Long id) {
         brandRepository.deleteById(id);
     }
 }

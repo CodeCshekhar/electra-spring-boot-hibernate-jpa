440de3181bd84a74be68b84d21800c5604aa9210
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-07 14:45:45 +0530

    commits

--- src/main/java/com/electra/controller/OrderController.java
+++ src/main/java/com/electra/controller/OrderController.java
@@ -2,57 +2,39 @@ package com.electra.controller;
 
 import com.electra.domain.Order;
 import com.electra.service.OrderService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/electra/order")
+@RequestMapping("/electra/orders")
 public class OrderController {
-    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
-
     @Autowired
     private OrderService orderService;
 
     @GetMapping
     public List<Order> getAllOrders() {
-        logger.info("Retrieving Orders from Get in Controller");
-        return orderService.getAllOrders();
+        return orderService.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
-        logger.info("Retrieving Order from Get by ID in Controller");
-        Order order = orderService.getOrderById(id);
-        if (order != null) {
-            return ResponseEntity.ok(order);
-        }
-        return ResponseEntity.notFound().build();
+        return orderService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public Order createOrder(@RequestBody Order order) {
-        logger.info("Creating Order from Post in Controller");
-        return orderService.createOrder(order);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody Order order) {
-        logger.info("Updating Order from Put in Controller");
-        Order updatedOrder = orderService.updateOrder(id, order);
-        if (updatedOrder != null) {
-            return ResponseEntity.ok(updatedOrder);
-        }
-        return ResponseEntity.notFound().build();
+        return orderService.save(order);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
-        logger.info("Deleting Order from delete in Controller");
-        orderService.deleteOrder(id);
+        orderService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }

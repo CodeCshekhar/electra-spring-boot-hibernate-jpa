96f7173242723651153e39b5d5bf96eb6b37032a
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-08 12:26:26 +0530

    commits

--- src/main/java/com/electra/service/OrderService.java
+++ src/main/java/com/electra/service/OrderService.java
@@ -2,53 +2,31 @@ package com.electra.service;
 
 import com.electra.domain.Order;
 import com.electra.repository.OrderRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
-    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
-
     @Autowired
     private OrderRepository orderRepository;
 
-    @Cacheable("getAllOrders")
-    public List<Order> getAllOrders() {
-        logger.info("Orders Retrieved in Services");
+    public List<Order> findAll() {
         return orderRepository.findAll();
     }
 
-    @Cacheable("getOrderById")
-    public Order getOrderById(Long id) {
-        logger.info("Order detected by ID in Services");
-        return orderRepository.findById(id).orElse(null);
+    public Optional<Order> findById(Long id) {
+        return orderRepository.findById(id);
     }
 
-    public Order createOrder(Order order) {
-        logger.info("Order Created in Services");
+    public Order save(Order order) {
         return orderRepository.save(order);
     }
 
-    public Order updateOrder(Long id, Order updatedOrder) {
-        Order order = orderRepository.findById(id).orElse(null);
-        if (order != null) {
-            order.setProduct(updatedOrder.getProduct());
-            order.setCustomer(updatedOrder.getCustomer());
-            order.setSupplier(updatedOrder.getSupplier());
-            order.setOrderDate(updatedOrder.getOrderDate());
-            logger.info("Order Updated in Services");
-            return orderRepository.save(order);
-        }
-        return null;
-    }
-
-    public void deleteOrder(Long id) {
-        logger.info("Order deleted");
+    public void delete(Long id) {
         orderRepository.deleteById(id);
     }
-
 }

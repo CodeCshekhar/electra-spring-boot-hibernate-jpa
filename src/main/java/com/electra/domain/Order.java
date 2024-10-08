fd771c0c67d0fca76cd4dd0a145e55eaaf279f4e
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-08 12:26:18 +0530

    commits

--- src/main/java/com/electra/domain/Order.java
+++ src/main/java/com/electra/domain/Order.java
@@ -25,29 +25,4 @@ public class Order {
             inverseJoinColumns = @JoinColumn(name = "product_id")
     )
     private List<Product> products;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
 }

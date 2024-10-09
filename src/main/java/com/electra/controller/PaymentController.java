d571329f1f1b798a0b697f6f6e39fe9ee883d2b6
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-09 14:37:10 +0530

    commits

--- src/main/java/com/electra/controller/PaymentController.java
+++ src/main/java/com/electra/controller/PaymentController.java
@@ -2,57 +2,38 @@ package com.electra.controller;
 
 import com.electra.domain.Payment;
 import com.electra.service.PaymentService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/electra/payment")
+@RequestMapping("/electra/payments")
 public class PaymentController {
-    private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
-
     @Autowired
     private PaymentService paymentService;
 
     @GetMapping
     public List<Payment> getAllPayments() {
-        logger.info("Retrieving Payments from Get in Controller");
-        return paymentService.getAllPayments();
+        return paymentService.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Payment> getPaymentById(@PathVariable Long id) {
-        logger.info("Retrieving Payment from Get by ID in Controller");
-        Payment payment = paymentService.getPaymentById(id);
-        if (payment != null) {
-            return ResponseEntity.ok(payment);
-        }
-        return ResponseEntity.notFound().build();
+        return paymentService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public Payment createPayment(@RequestBody Payment payment) {
-        logger.info("Creating Payment from Post in Controller");
-        return paymentService.createPayment(payment);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Payment> updatePayment(@PathVariable Long id, @RequestBody Payment payment) {
-        logger.info("Updating Payment from Put in Controller");
-        Payment updatedPayment = paymentService.updatePayment(id, payment);
-        if (updatedPayment != null) {
-            return ResponseEntity.ok(updatedPayment);
-        }
-        return ResponseEntity.notFound().build();
+        return paymentService.save(payment);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePayment(@PathVariable Long id) {
-        logger.info("Deleting Payment from delete in Controller");
-        paymentService.deletePayment(id);
+        paymentService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }

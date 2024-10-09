48c1b2df0102ee48dcfa77b11ca8be64a648b57f
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-09 14:37:19 +0530

    commits

--- src/main/java/com/electra/service/PaymentService.java
+++ src/main/java/com/electra/service/PaymentService.java
@@ -2,52 +2,31 @@ package com.electra.service;
 
 import com.electra.domain.Payment;
 import com.electra.repository.PaymentRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class PaymentService {
-    private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
-
     @Autowired
     private PaymentRepository paymentRepository;
 
-    @Cacheable("getAllPayments")
-    public List<Payment> getAllPayments() {
-        logger.info("Payments Retrieved in Services");
+    public List<Payment> findAll() {
         return paymentRepository.findAll();
     }
 
-    @Cacheable("getPaymentById")
-    public Payment getPaymentById(Long id) {
-        logger.info("Payment detected by ID in Services");
-        return paymentRepository.findById(id).orElse(null);
+    public Optional<Payment> findById(Long id) {
+        return paymentRepository.findById(id);
     }
 
-    public Payment createPayment(Payment payment) {
-        logger.info("Payment Created in Services");
+    public Payment save(Payment payment) {
         return paymentRepository.save(payment);
     }
 
-    public Payment updatePayment(Long id, Payment updatedPayment) {
-        Payment payment = paymentRepository.findById(id).orElse(null);
-        if (payment != null) {
-            payment.setAmount(updatedPayment.getAmount());
-            payment.setPaymentDate(updatedPayment.getPaymentDate());
-            payment.setCustomer(updatedPayment.getCustomer());
-            payment.setOrder(updatedPayment.getOrder());
-            logger.info("Payment Updated in Services");
-            return paymentRepository.save(payment);
-        }
-        return null;
-    }
-
-    public void deletePayment(Long id) {
-        logger.info("Payment deleted");
+    public void delete(Long id) {
         paymentRepository.deleteById(id);
     }
 }

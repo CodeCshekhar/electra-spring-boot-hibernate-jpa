24dd06b3fadfd26b35d061de62b2bdcc40bcd3c6
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-10 11:22:54 +0530

    commits

--- src/main/java/com/electra/controller/AddressController.java
+++ src/main/java/com/electra/controller/AddressController.java
@@ -1,55 +1,39 @@
 package com.electra.controller;
 
-import com.electra.domain.Address;
+import com.electra.model.AddressModel;
 import com.electra.service.AddressService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/Electra/addresses")
+@RequestMapping(value = "/electra")
 public class AddressController {
-    private static final Logger logger = LoggerFactory.getLogger(AddressController.class);
 
-    @Autowired
+    @Resource(name = "addressService")
     private AddressService addressService;
 
-    @GetMapping
-    public ResponseEntity<List<Address>> retrieveAddresses() {
-        logger.info("Retrieving all addresses");
-        return ResponseEntity.ok(addressService.retrieveAddresses());
+
+    @GetMapping("/addresses")
+    public List<AddressModel> getAddresses(){
+        return addressService.getAllAddresses();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Address> getAddressById(@PathVariable long id) {
-        logger.info("Retrieving address by ID: {}", id);
-        Address address = addressService.search(id).orElse(null);
-        if (address != null) {
-            return ResponseEntity.ok(address);
-        }
-        return ResponseEntity.notFound().build();
+    @GetMapping("/address/{id}")
+    public AddressModel getAddress(@PathVariable Long id){
+        return addressService.getaddressById(id);
     }
 
-    @PostMapping
-    public ResponseEntity<Address> createAddress(@RequestBody Address address) {
-        logger.info("Creating address");
-        return ResponseEntity.ok(addressService.storeAddress(address));
+
+    @PostMapping("/address")
+    public AddressModel saveAddress(final @RequestBody AddressModel addressModel){
+        return addressService.saveAddress(addressModel);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Address> updateAddress(@PathVariable long id, @RequestBody Address address) {
-        logger.info("Updating address with ID: {}", id);
-        return ResponseEntity.ok(addressService.updateAddress(address));
-    }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAddress(@PathVariable long id) {
-        logger.info("Deleting address with ID: {}", id);
-        addressService.deleteAddress(id);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/address/{id}")
+    public Boolean deleteAddress(@PathVariable Long id){
+        return addressService.deleteAddress(id);
     }
 }

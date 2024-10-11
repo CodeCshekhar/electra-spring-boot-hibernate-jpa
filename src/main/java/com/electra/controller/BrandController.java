02319be2f2483adc441e7d5fd51d66f90e9d7797
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-11 15:17:48 +0530

    commits

--- src/main/java/com/electra/controller/BrandController.java
+++ src/main/java/com/electra/controller/BrandController.java
@@ -1,55 +1,36 @@
 package com.electra.controller;
 
-import com.electra.domain.Brand;
+import com.electra.model.BrandModel;
 import com.electra.service.BrandService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/Electra/brands")
+@RequestMapping(value = "/electra")
 public class BrandController {
-    private static final Logger logger = LoggerFactory.getLogger(BrandController.class);
 
-    @Autowired
+    @Resource(name = "brandService")
     private BrandService brandService;
 
-    @GetMapping
-    public ResponseEntity<List<Brand>> retrieveBrands() {
-        logger.info("Retrieving all brands");
-        return ResponseEntity.ok(brandService.retrieveBrands());
+    @GetMapping("/brands")
+    public List<BrandModel> getBrands(){
+        return brandService.getAllBrands();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Brand> getBrandById(@PathVariable long id) {
-        logger.info("Retrieving brand by ID: {}", id);
-        Brand brand = brandService.search(id).orElse(null);
-        if (brand != null) {
-            return ResponseEntity.ok(brand);
-        }
-        return ResponseEntity.notFound().build();
+    @GetMapping("/brand/{id}")
+    public BrandModel getBrand(@PathVariable Long id){
+        return brandService.getBrandById(id);
     }
 
-    @PostMapping
-    public ResponseEntity<Brand> createBrand(@RequestBody Brand brand) {
-        logger.info("Creating brand");
-        return ResponseEntity.ok(brandService.storeBrand(brand));
+    @PostMapping("/brand")
+    public BrandModel saveBrand(final @RequestBody BrandModel brandModel){
+        return brandService.saveBrand(brandModel);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Brand> updateBrand(@PathVariable long id, @RequestBody Brand brand) {
-        logger.info("Updating brand with ID: {}", id);
-        return ResponseEntity.ok(brandService.updateBrand(brand));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBrand(@PathVariable long id) {
-        logger.info("Deleting brand with ID: {}", id);
-        brandService.deleteBrand(id);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/brand/{id}")
+    public Boolean deleteBrand(@PathVariable Long id){
+        return brandService.deleteBrand(id);
     }
 }

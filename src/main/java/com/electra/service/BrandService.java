01e5f0a3591fdf9f88f865404a5c004a9188b1bd
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-12 18:55:44 +0530

    commits

--- src/main/java/com/electra/service/BrandService.java
+++ src/main/java/com/electra/service/BrandService.java
@@ -1,43 +1,13 @@
 package com.electra.service;
 
-import com.electra.domain.Brand;
-import com.electra.repository.BrandRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import com.electra.model.BrandModel;
+
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class BrandService {
-    private static final Logger logger = LoggerFactory.getLogger(BrandService.class);
-
-    @Autowired
-    private BrandRepository repo;
-
-    public List<Brand> retrieveBrands() {
-        logger.info("Inside BrandService.retrieveBrands()");
-        return repo.findAll();
-    }
-
-    public Brand storeBrand(Brand brand) {
-        logger.info("Inside BrandService.storeBrand()");
-        return repo.save(brand);
-    }
-
-    public void deleteBrand(long id) {
-        logger.info("Inside BrandService.deleteBrand()");
-        repo.deleteById(id);
-    }
-
-    public Optional<Brand> search(long id) {
-        logger.info("Inside BrandService.search()");
-        return repo.findById(id);
-    }
-
-    public Brand updateBrand(Brand brand) {
-        logger.info("Inside BrandService.updateBrand()");
-        return repo.save(brand);
-    }
+public interface BrandService {
+    BrandModel saveBrand(BrandModel brand);
+    boolean deleteBrand(final Long brandId);
+    List<BrandModel> getAllBrands();
+    BrandModel getBrandById(final Long brandId);
 }
--- src/main/java/com/electra/service/CustomerService.java
+++ src/main/java/com/electra/service/CustomerService.java
@@ -1,43 +1,13 @@
 package com.electra.service;
 
-import com.electra.domain.Customer;
-import com.electra.repository.CustomerRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.electra.model.CustomerModel;
+
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class CustomerService {
-    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+public interface CustomerService {
 
-    @Autowired
-    private CustomerRepository customerRepo;
-
-    public List<Customer> retrieveCustomers() {
-        logger.info("Inside CustomerService.retrieveCustomers()");
-        return customerRepo.findAll();
-    }
-
-    public Customer storeCustomer(Customer customer) {
-        logger.info("Inside CustomerService.storeCustomer()");
-        return customerRepo.save(customer);
-    }
-
-    public void deleteCustomer(long id) {
-        logger.info("Inside CustomerService.deleteCustomer()");
-        customerRepo.deleteById(id);
-    }
-
-    public Optional<Customer> search(long customerId) {
-        logger.info("Inside CustomerService.search()");
-        return customerRepo.findById(customerId);
-    }
-
-    public Customer updateCustomer(Customer customer) {
-        logger.info("Inside CustomerService.updateCustomer()");
-        return customerRepo.save(customer);
-    }
+    CustomerModel saveCustomer(CustomerModel customer);
+    boolean deleteCustomer(final Long customerId);
+    List<CustomerModel> getAllCustomers();
+    CustomerModel getCustomerById(final Long customerId);
 }

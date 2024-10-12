1b81e8752c31b5ee3d7a08f01dd923c18def3cf9
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-12 18:55:20 +0530

    commits

--- src/main/java/com/electra/domain/Customer.java
+++ src/main/java/com/electra/domain/Customer.java
@@ -1,23 +1,33 @@
 package com.electra.domain;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Entity
-@Table(name = "customers")
-@Data
+@Getter
+@Setter
+@Entity(name = "Customer")
+@ToString
 @NoArgsConstructor
-@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Exclude
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
 
     private String name;
 
+    @ToString.Exclude
     private String email;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id")
     private Address address;
+
+    public Customer(long id, String name, String email, Address address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
 }

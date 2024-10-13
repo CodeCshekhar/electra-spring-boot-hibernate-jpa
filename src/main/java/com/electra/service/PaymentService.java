353d56086546e12b82492e74a2aed56821d2c2b8
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-10-13 13:42:25 +0530

    commits

--- src/main/java/com/electra/service/PaymentService.java
+++ src/main/java/com/electra/service/PaymentService.java
@@ -1,43 +1,13 @@
 package com.electra.service;
 
-import com.electra.domain.Payment;
-import com.electra.repository.PaymentRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import com.electra.model.PaymentModel;
+
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class PaymentService {
-    private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
-
-    @Autowired
-    private PaymentRepository repo;
-
-    public List<Payment> retrievePayments() {
-        logger.info("Inside PaymentService.retrievePayments()");
-        return repo.findAll();
-    }
-
-    public Payment storePayment(Payment payment) {
-        logger.info("Inside PaymentService.storePayment()");
-        return repo.save(payment);
-    }
-
-    public void deletePayment(long id) {
-        logger.info("Inside PaymentService.deletePayment()");
-        repo.deleteById(id);
-    }
-
-    public Optional<Payment> search(long id) {
-        logger.info("Inside PaymentService.search()");
-        return repo.findById(id);
-    }
-
-    public Payment updatePayment(Payment payment) {
-        logger.info("Inside PaymentService.updatePayment()");
-        return repo.save(payment);
-    }
+public interface PaymentService {
+    PaymentModel savePayment(PaymentModel payment);
+    boolean deletePayment(final Long paymentId);
+    List<PaymentModel> getAllPayments();
+    PaymentModel getPaymentById(final Long paymentId);
 }

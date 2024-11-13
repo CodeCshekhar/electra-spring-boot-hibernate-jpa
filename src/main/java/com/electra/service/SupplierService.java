be36936af69b8e659f80bf7d7dd470f9389e62de
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-11-13 11:26:18 +0530

    commits

--- src/main/java/com/electra/service/SupplierService.java
+++ src/main/java/com/electra/service/SupplierService.java
@@ -1,43 +1,13 @@
 package com.electra.service;
 
-import com.electra.domain.Supplier;
-import com.electra.repository.SupplierRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.electra.model.SupplierModel;
+
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class SupplierService {
-    private static final Logger logger = LoggerFactory.getLogger(SupplierService.class);
+public interface SupplierService {
+    SupplierModel saveSupplier(SupplierModel supplier);
+    boolean deleteSupplier(final Long supplierId);
+    List<SupplierModel> getAllSuppliers();
+    SupplierModel getSupplierById(final Long supplierId);
 
-    @Autowired
-    private SupplierRepository repo;
-
-    public List<Supplier> retrieveSuppliers() {
-        logger.info("Inside SupplierService.retrieveSuppliers()");
-        return repo.findAll();
-    }
-
-    public Supplier storeSupplier(Supplier supplier) {
-        logger.info("Inside SupplierService.storeSupplier()");
-        return repo.save(supplier);
-    }
-
-    public void deleteSupplier(long id) {
-        logger.info("Inside SupplierService.deleteSupplier()");
-        repo.deleteById(id);
-    }
-
-    public Optional<Supplier> search(long id) {
-        logger.info("Inside SupplierService.search()");
-        return repo.findById(id);
-    }
-
-    public Supplier updateSupplier(Supplier supplier) {
-        logger.info("Inside SupplierService.updateSupplier()");
-        return repo.save(supplier);
-    }
 }
